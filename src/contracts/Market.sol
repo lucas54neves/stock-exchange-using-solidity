e5e14e5724a837718ad52f158894de2f40399b37
Lucas Neves <lucas54neves@gmail.com>
2022-03-02 23:31:56 -0300

    feat: add function to update orders

--- src/contracts/Market.sol
+++ src/contracts/Market.sol
@@ -3,112 +3,222 @@ pragma solidity ^0.8.3;
 
 contract Market {
     struct Order {
+        uint256 index;
         address createdBy;
-        uint createdAt;
+        uint256 createdAt;
         string assetCode;
-        uint targetPricePerShare;
-        uint numberOfShares;
+        uint256 targetPricePerShare;
+        uint256 numberOfShares;
         bool acceptsFragmenting;
     }
 
     struct Transaction {
+        uint256 index;
         address seller;
         address buyer;
-        uint createdAt;
-        uint sellTime;
-        uint buyTime;
+        uint256 sellOrderIndex;
+        uint256 buyOrderIndex;
         string assetCode;
-        uint pricePerShare;
-        uint numberOfShares;
+        uint256 createdAt;
+        uint256 pricePerShare;
+        uint256 numberOfShares;
+    }
+
+    struct GetOrdersReturn {
+        uint256 size;
+        mapping(uint256 => Order) orders;
     }
 
     Order[] private sellOrders;
     Order[] private buyOrders;
     Transaction[] private transactions;
-    
-    function compareStrings(string memory a, string memory b) private pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+
+    function compareStrings(string memory a, string memory b)
+        private
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked((a))) ==
+            keccak256(abi.encodePacked((b))));
     }
 
-    function createOrder(address userAddress, string memory assetCode, uint targetPricePerShare, uint numberOfShares, bool acceptsFragmenting) public view returns (Order memory) {
-        return Order({
-            sellerOrBuyerAddress: userAddress,
-            assetCode: assetCode,
-            targetPricePerShare: targetPricePerShare,
-            numberOfShares: numberOfShares,
-            createdAt: block.timestamp,
-            acceptsFragmenting: acceptsFragmenting
-        });
+    function createOrder(
+        uint256 orderIndex,
+        address userAddress,
+        string memory assetCode,
+        uint256 targetPricePerShare,
+        uint256 numberOfShares,
+        bool acceptsFragmenting
+    ) public view returns (Order memory) {
+        return
+            Order({
+                index: orderIndex,
+                createdBy: userAddress,
+                assetCode: assetCode,
+                targetPricePerShare: targetPricePerShare,
+                numberOfShares: numberOfShares,
+                createdAt: block.timestamp, // require view-type function
+                acceptsFragmenting: acceptsFragmenting
+            });
     }
 
-    function createTransaction(address seller, address buyer, uint createdAt, uint sellTime, uint buyTime, string assetCode, uint pricePerShare, uint numberOfShares) {
-        return Transaction({
-            seller: seller,
-            buyer: buyer,
-            createdAt: createdAt,
-            sellTime: sellTime,
-            buyTime: buyTime,
-            assetCode: assetCode,
-            pricePerShare: priceOfShares,
-            numberOfShares: numberOfShares
-        });
+    function createTransaction(
+        address seller,
+        address buyer,
+        uint256 createdAt,
+        uint256 sellOrderIndex,
+        uint256 buyOrderIndex,
+        string memory assetCode,
+        uint256 pricePerShare,
+        uint256 numberOfShares
+    ) public view returns (Transaction memory) {
+        return
+            Transaction({
+                index: transactions.length + 1,
+                seller: seller,
+                buyer: buyer,
+                sellOrderIndex: sellOrderIndex,
+                buyOrderIndex: buyOrderIndex,
+                assetCode: assetCode,
+                pricePerShare: pricePerShare,
+                createdAt: createdAt,
+                numberOfShares: numberOfShares
+            });
     }
 
-    function addOrder(address userAddress, string memory assetCode, uint targetPricePerShare, uint numberOfShares, bool isSell, bool acceptsFragmenting) public {
-        Order memory order = createOrder(userAddress, assetCode, targetPricePerShare, numberOfShares, acceptsFragmenting);
+    function addOrder(
+        address userAddress,
+        string memory assetCode,
+        uint256 targetPricePerShare,
+        uint256 numberOfShares,
+        bool isSell,
+        bool acceptsFragmenting
+    ) public {
+        uint256 orderIndex;
+
+        if (isSell) {
+            orderIndex = sellOrders.length + 1;
+        } else {
+            orderIndex = buyOrders.length + 1;
+        }
+
+        Order memory order = createOrder(
+            orderIndex,
+            userAddress,
+            assetCode,
+            targetPricePerShare,
+            numberOfShares,
+            acceptsFragmenting
+        );
 
         if (isSell) {
             addSellOrder(order);
         } else {
-            addBuyOrder(buyer);
+            addBuyOrder(order);
         }
     }
 
     // Todo: insert orderly
-    function addSellOrder(Order order) {
+    function addSellOrder(Order memory order) public {
         sellOrders.push(order);
     }
 
     // Todo: insert orderly
-    function addBuyOrder(Order order) {
+    function addBuyOrder(Order memory order) public {
         buyOrders.push(order);
     }
 
-    function getSellOrders() public view returns (Order[] memory) {
+    function readSellOrders() public view returns (Order[] memory) {
         return sellOrders;
     }
 
-    function getBuyOrders() public view returns (Order[] memory) {
+    function readBuyOrders() public view returns (Order[] memory) {
         return buyOrders;
     }
 
-    function getTransactions() public view returns (Transaction[] memory) {
+    function readTransactions() public view returns (Transaction[] memory) {
         return transactions;
     }
 
-    function getSellOrder(string assetCode, uint pricePerShare) {
-        for (uint i = 0; i < getSellOrders().length; i++) {
-            Order memory sellOrder = getSellOrders()[i];
+    function readOrdersFromUser(address userAddress, bool isSellOrder)
+        public
+        view
+        returns (Order[] memory)
+    {
+        Order[] memory orders;
 
-            if (compareStrings(sellOrder.assetCode, assetCode)) {
-                if (sellOrder.targetPricePerShare <= pricePerShare) {
-                    return sellOrder;
+        if (isSellOrder) {
+            for (uint256 i = 0; i <= sellOrders.length; i++) {
+                if (sellOrders[i].createdBy == userAddress) {
+                    orders[i] = sellOrders[i];
+                }
+            }
+        } else {
+            for (uint256 i = 0; i <= buyOrders.length; i++) {
+                if (buyOrders[i].createdBy == userAddress) {
+                    orders[i] = buyOrders[i];
                 }
             }
         }
+
+        return orders;
     }
 
-    function checkTransactionConflict(bool sellerAcceptsToFragment, bool buyerAcceptsToFragment, bool selletHasTheMostQuantity) public pure returns (bool) {
-        if (!sellerAcceptsToFragment && selletHasTheMostQuantity) {
+    function readTransactionsFromUser(address userAddress)
+        public
+        view
+        returns (Transaction[] memory)
+    {
+        Transaction[] memory transactionsFromUser;
+
+        for (uint256 i = 0; i <= transactions.length; i++) {
+            if (
+                transactions[i].seller == userAddress ||
+                transactions[i].buyer == userAddress
+            ) {
+                transactionsFromUser[i] = transactions[i];
+            }
+        }
+
+        return transactionsFromUser;
+    }
+
+    function updateOrder(
+        address userAddress,
+        bool isSellOrder,
+        uint256 orderIndex,
+        uint256 newNumberOfShare,
+        uint256 newTargetPricePerShare
+    ) public view returns (Order memory) {
+        Order[] memory orders = readOrdersFromUser(userAddress, isSellOrder);
+
+        assert(orders.length > 0);
+
+        for (uint256 i = 0; i < orders.length; i++) {
+            if (orders[i].index == orderIndex) {
+                orders[i].numberOfShares = newNumberOfShare;
+                orders[i].targetPricePerShare = newTargetPricePerShare;
+
+                return orders[i];
+            }
+        }
+
+        revert("Order not found.");
+    }
+
+    function checkTransactionConflict(
+        bool sellerAcceptsToFragment,
+        bool buyerAcceptsToFragment,
+        bool sellerHasTheMostQuantity
+    ) public pure returns (bool) {
+        if (!sellerAcceptsToFragment && sellerHasTheMostQuantity) {
             return true;
         }
 
-        if (!buyerAcceptsToFragment && !selletHasTheMostQuantity) {
+        if (!buyerAcceptsToFragment && !sellerHasTheMostQuantity) {
             return true;
         }
 
         return false;
     }
-
-    function
 }
