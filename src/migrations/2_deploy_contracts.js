0fa00a214aaba7901bc925ec7fa6aea63faba183
Lucas Neves <lucas54neves@gmail.com>
2022-06-01 21:50:27 -0300

    feat: add contract from a linked list of orders

--- src/migrations/2_deploy_contracts.js
+++ src/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const StockExchange = artifacts.require("StockExchange");
+const LinkedListOfOrders = artifacts.require("LinkedListOfOrders");
 
 module.exports = function (deployer) {
   deployer.deploy(StockExchange);
+  deployer.deploy(LinkedListOfOrders);
 };
